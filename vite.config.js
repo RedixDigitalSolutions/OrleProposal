a3fc7be2304f08ccd17adc760e84b29075ab30bf
PeXxa <yassinejeridi23@gmail.com>
2025-09-30 02:45:11 +0100

    orle_proposal

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    port: 3000,
-    open: true
-  }
+  base: '/OrleProposal/',   // <-- set to '/<repo>/'
+  server: { port: 3000, open: true }
 })
